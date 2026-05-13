52792c45b216a2b6d3d9f7f9184082e841bd8b9f
satish.basavanneppa <satish.basavanneppa@graphnethealth.com>
2026-05-13 13:16:28 +0100

    Add CD pipeline

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
         {
             'time': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             'host': socket.gethostname(),
-            'message': 'New field - upd3' 
+            'message': 'Hello world' 
         }
     )
 
